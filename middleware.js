def753e6552d1b23747e4d69d146ef929a63f9c2
Jat775 <gurjarjatin834@gmail.com>
2025-11-28 10:36:46 +0530

    Initial commit

---
+++ middleware.js
@@ -0,0 +1,41 @@
+import arcjet, { createMiddleware, detectBot, shield } from "@arcjet/next";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+const isProtectedRoute = createRouteMatcher([
+  "/dashboard(.*)",
+  "/account(.*)",
+  "/transaction(.*)",
+]);
+
+// ArcJet
+const aj = arcjet({
+  key: process.env.ARCJET_KEY,
+  rules: [
+    shield({ mode: "LIVE" }),
+    detectBot({
+      mode: "LIVE",
+      allow: ["CATEGORY:SEARCH_ENGINE", "GO_HTTP"],
+    }),
+  ],
+});
+
+// Clerk
+const clerk = clerkMiddleware(async (auth, req) => {
+  const { userId } = await auth();
+  if (!userId && isProtectedRoute(req)) {
+    const { redirectToSignIn } = await auth();
+    return redirectToSignIn();
+  }
+  return NextResponse.next();
+});
+
+// Chain middlewares
+export default createMiddleware(aj, clerk);
+
+// MUST MATCH ALL PAGES
+export const config = {
+  matcher: [
+    "/((?!_next|.*\\..*|favicon.ico).*)",
+  ],
+};
